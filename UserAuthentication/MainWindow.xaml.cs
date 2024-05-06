929192af218f9f837b9b1a0614a163ee7b9fac84
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2024-05-06 17:06:36 +0300

    Add project files.

---
+++ UserAuthentication/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserAuthentication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textUsername.Text.Trim();
+            string password = textPassword.Password.Trim();
+
+            if (login.Length < 4)
+            {
+                textUsername.ToolTip = "Введите больше трех символов.";
+                textUsername.Background = Brushes.DarkRed;
+            }
+            else if (password.Length < 4)
+            {
+                textPassword.ToolTip = "Введите больше трех символов.";
+                textPassword.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textUsername.ToolTip = "";
+                textUsername.Background = Brushes.Transparent;
+                textPassword.ToolTip = "";
+                textPassword.Background = Brushes.Transparent;
+
+                MessageBox.Show("Ok");
+            }
+        }
+
+        private void cancel_button_Click(object sender, RoutedEventArgs e)
+        {
+            textUsername.Clear();
+            textPassword.Clear();
+        }
+    }
+}
